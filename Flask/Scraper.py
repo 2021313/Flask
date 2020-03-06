ed6eee330fb3dd5ce335b817ad8fbf8c005fb1a1
DPainton0912 <dgpainton@gmail.com>
2020-03-06 14:16:45 +1300

    Flask update so it scrapes trademe and shows results

---
+++ Flask/Scraper.py
@@ -0,0 +1,35 @@
+#Scraper
+import requests
+from bs4 import BeautifulSoup
+def PageScraper():
+    cnt = 1
+    page = 1 
+    results = []
+    minprice = None
+    maxprice = None
+    while cnt > 0:
+        response = requests.get("https://www.trademe.co.nz/gaming/playstation-4/consoles?buy=buynow&page=" + str(page))
+        soup = BeautifulSoup(response.text,"html.parser")
+        itemlist = soup.findAll("div", {"class":"supergrid-listing"})
+        cnt = len(itemlist)
+ 
+        #print("Page ", page, ": Cnt ", cnt)
+        for item in itemlist:
+            try:
+                item_title = item.find("div", {"class":"title"}).text.strip()
+                item_price = item.find("div", {"class":"listingBuyNowPrice"}).text.strip()
+                item_price = float(item_price.strip("$"))
+                results.append({"title":item_title,"price":item_price})
+                #print(item_title, ",", item_price)
+                if minprice == None or item_price < minprice:
+                    minprice = item_price
+                if maxprice == None or item_price > maxprice:
+                    maxprice = item_price
+            except:
+                print("Something went wrong!")
+        page = page + 1
+    return results, maxprice, minprice
+#print(results)
+#print(len(results))
+#print(minprice)
+#print(maxprice)
---
+++ Flask/test_Flask_Scraper.py
@@ -0,0 +1,26 @@
+import pytest
+from Scraper import PageScraper
+def test_PageScraper_List():
+    containsTitle = False
+    containsPrice = False
+    consolelist, a, b = PageScraper()
+    print(a, b)
+    print(len(consolelist))
+    i = 0
+    for i in range(len(consolelist)):
+        try:
+            print(consolelist[i])
+            if (len(consolelist[i]['title']) > 0):
+                containsTitle = True
+            if (consolelist[i]['price'] > 0):
+                containsPrice = True
+                break
+        except:
+            print("Fail")
+            i + 1
+    assert containsTitle == True and containsPrice == True
+
+def test_PageScraper_Min_Max():
+    z, maxprice, minprice = PageScraper()
+    print(z[0])
+    assert minprice > 0 and maxprice > 0
